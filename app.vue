d1067cbaeb870cec8f4555b5c38552dbd4e9bf70
fikrialwan <fikriar93@gmail.com>
2024-10-11 22:23:19 +0700

    feat(main-page): add hero

--- app.vue
+++ app.vue
@@ -1,5 +1,9 @@
 <template>
-  <div class="dark bg-background text-foreground min-h-screen">
-    <NuxtPage />
+  <div class="dark bg-background text-foreground flex flex-col min-h-dvh">
+    <div
+      class="relative w-full mx-auto max-w-screen-lg px-6 lg:px-8 flex flex-col grow py-8 gap-8 md:gap-10 md:py-10 lg:gap-12 lg:py-12"
+    >
+      <NuxtPage />
+    </div>
   </div>
 </template>
